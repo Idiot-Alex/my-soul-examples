2632e00b4bdfb152a78983ff4f6774fbef0fd997
Eleven <idiotalex@163.com>
2021-01-17 03:35:29 +0800

    add http-demo module

---
+++ http-demo/src/main/java/com/hotstrip/httpdemo/dto/User.java
@@ -0,0 +1,9 @@
+package com.hotstrip.httpdemo.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+}
---
+++ http-demo/src/main/java/com/hotstrip/httpdemo/web/UserController.java
@@ -0,0 +1,35 @@
+package com.hotstrip.httpdemo.web;
+
+import com.hotstrip.httpdemo.dto.User;
+import org.dromara.soul.client.springmvc.annotation.SoulSpringMvcClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@SoulSpringMvcClient(path = "/user/*")
+public class UserController {
+
+    @GetMapping(value = "/user/{id}")
+    public Object getUserById(@PathVariable(value = "id") Long id) {
+        User info = new User();
+        info.setId(id);
+        info.setName("name" + id);
+        return info;
+    }
+
+    @GetMapping(value = "/user/list")
+    public Object getUserList() {
+        List<User> list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            User info = new User();
+            info.setId(Long.valueOf(i));
+            info.setName("name" + i);
+            list.add(info);
+        };
+        return list;
+    }
+}
