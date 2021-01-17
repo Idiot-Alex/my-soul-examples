b5133d485d0b4bc06adc4ee9b6377ed7ef2b5413
Eleven <idiotalex@163.com>
2021-01-17 20:03:14 +0800

    update userController soul api config

--- http-demo/src/main/java/com/hotstrip/httpdemo/web/UserController.java
+++ http-demo/src/main/java/com/hotstrip/httpdemo/web/UserController.java
@@ -9,11 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * user controller
+ * @author Hotstrip
+ */
 @RestController
-@SoulSpringMvcClient(path = "/user/*")
 public class UserController {
 
+    /**
+     * get user by id
+     * @param id
+     * @return
+     */
     @GetMapping(value = "/user/{id}")
+    @SoulSpringMvcClient(path = "/user/*", desc = "get user by id")
     public Object getUserById(@PathVariable(value = "id") Long id) {
         User info = new User();
         info.setId(id);
@@ -21,7 +30,12 @@ public class UserController {
         return info;
     }
 
+    /**
+     * get user list
+     * @return
+     */
     @GetMapping(value = "/user/list")
+    @SoulSpringMvcClient(path = "/user/list", desc = "get user list")
     public Object getUserList() {
         List<User> list = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
